27ad246b7c2383d793c327fb4a49331bcd9a9c54
Student-JennaJe <94848871+atpagio@users.noreply.github.com>
2022-01-13 14:32:08 +0000

    Initial commit

---
+++ Main.java
@@ -0,0 +1,13 @@
+import java.util.*;
+import java.io.*;
+
+class Main {
+  public static void main(String[] args) throws IOException {
+    String token="";
+    File fileName=new File("Hail.txt");
+    Scanner inputFile=new Scanner(fileName);
+
+    //use while loop to read tokens from the file Hail.txt
+    while(inputFile.hasNext())
+  }
+}
